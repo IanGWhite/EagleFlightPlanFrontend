a81228bdc01215449f9add8c74b813d4bc95f995
ChargeChan <chandlerbilbo@gmail.com>
2025-02-07 16:22:37 -0600

    left bar set up + progress on center timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,13 +17,12 @@ const goToInfo = () => {
 
 <template>
     <v-app class="rounded rounded-md">
-      <!-- <v-app-bar color="surface-variant" title="Application bar"></v-app-bar> -->
-  
-      <v-navigation-drawer>
+      <!-- LEFT SIDE DRAWER -->
+      <v-navigation-drawer permanent class="navigation-drawer">
         <v-list style="margin: 10px;">
           <v-list-item></v-list-item>
-          <v-card title="Upcoming Events" variant="tonal">
-            
+            <v-card variant="tonal">
+            <v-card-title class="text-center">Upcoming Events</v-card-title>
           <v-list-item class="card-list-item">
             <v-row no-gutters >
               <v-col>
@@ -57,14 +56,16 @@ const goToInfo = () => {
             </v-row>
           </v-list-item>
 
-          <v-card-text opacity="0.5" class="text-right">
-            <a href="www.google.com/" class="text-decoration-none">See full calendar</a>
+          <v-card-text class="card-link-text-wrapper">
+            <a href="www.google.com/" class="card-link-text">See full calendar</a>
           </v-card-text>
 
           </v-card>
-          <v-list-item></v-list-item>
-          <v-card title="Experiences" variant="tonal">
-            
+          <v-list-item></v-list-item> <!-- SPACE IN BETWEEN CARDS -->
+
+          <!--              EXPERIENCES CARD               -->
+          <v-card variant="tonal">
+            <v-card-title class="text-center">Experiences</v-card-title>
             <v-list-item class="card-list-item">
               <v-row no-gutters >
                 <v-col>
@@ -98,26 +99,71 @@ const goToInfo = () => {
               </v-row>
             </v-list-item>
             
-            <v-card-text opacity="0.5" class="text-right">
-              <a href="www.google.com/" class="text-decoration-none">See all</a>
+            <v-card-text class="card-link-text-wrapper">
+              <a href="www.google.com/" class="card-link-text">See all</a>
             </v-card-text>
   
             </v-card>
           
         </v-list>
       </v-navigation-drawer>
+            <!-- RIGHT DRAWER -->
+      <v-navigation-drawer location="right" permanent class="navigation-drawer">
+        <v-list style="margin: 10px;">
+          <v-list-item></v-list-item>
+           <!-- POINTS CARD -->
+            <v-card variant="tonal">
+            <v-card-title class="text-center">Your Points</v-card-title>
+          <v-card-text class="points-text">
+            500
+          </v-card-text>
+
+          <v-card-text class="card-link-text-wrapper">
+            <a href="www.google.com/" class="card-link-text">See Shop</a>
+          </v-card-text>
+
+          </v-card>
+          <v-list-item></v-list-item> <!-- SPACE IN BETWEEN CARDS -->
+
+          <!--              BADGES CARD               -->
+          <v-card variant="tonal">
+            <v-card-title class="text-center">Badges</v-card-title>
+            <v-list-item >
+              <v-row no-gutters>
+                <v-col class="d-flex child-flex">
+                  <v-img src="../src/assets/badge-placeholder.png" cover></v-img>
+                </v-col>
+                <v-col class="d-flex child-flex">
+                  <v-img src="../src/assets/badge-placeholder.png" cover></v-img>
+                </v-col>
+                <v-col class="d-flex child-flex">
+                  <v-img src="../src/assets/badge-placeholder.png" cover></v-img>
+                </v-col>
+                
+              </v-row>
+            </v-list-item>
   
-      <v-navigation-drawer location="right">
-        <v-list>
-          <v-list-item title="Drawer right"></v-list-item>
+            
+            
+            <v-card-text class="card-link-text-wrapper">
+              <a href="www.google.com/" class="card-link-text">See all</a>
+            </v-card-text>
+            </v-card>
         </v-list>
       </v-navigation-drawer>
-  
-      <v-main
-        class="d-flex align-center justify-center"
-        style="min-height: 300px"
-      >
-        Main Content
+
+  <!--            MAIN            -->
+      <v-main>
+       
+        <v-card class="main-tasks" variant="tonal">
+          <v-timeline>
+            <v-timeline-item>
+              timeline 1
+            </v-timeline-item>
+          </v-timeline>
+
+        </v-card>
+
       </v-main>
     </v-app>
   </template>
@@ -132,5 +178,35 @@ const goToInfo = () => {
 }
 .card-list-item{
   min-height: 10px;
+}
+.card-link-text{
+  opacity: 0.5;
+  color: black;
+  text-decoration: none;
+}
+
+.card-link-text-wrapper{
+  text-align: right;
+  padding-top: 1%;
+  padding-bottom: 3%;
+}
+
+.navigation-drawer{
+  border: 0;
+  /* padding-left: 2%;
+  padding-right: 2%; */
+}
+.points-text{
+  text-align: center;
+  font-size: 3rem;
+  font-weight:bold;
+  line-height: 0.7;
+  padding-top: 3%;
+}
+.badge-image{
+  max-height:10%;
+}
+.main-tasks{
+
 }
 </style>
