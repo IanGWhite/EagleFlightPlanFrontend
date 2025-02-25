58686413e179409ff8daea19063526bb45eb1ee3
ChargeChan <chandlerbilbo@gmail.com>
2025-02-25 17:12:11 -0600

    final attempt at v-calendar
    Probably switching to another calendar

--- src/views/StudentCalendar.vue
+++ src/views/StudentCalendar.vue
@@ -8,15 +8,18 @@ import { useDate } from 'vuetify';
 const router = useRouter();
 const formatter = useDate();
 
-const calDialog = ref(true);
+const calDialog = ref(false);
 const currentItem = ref(0);
 
 const events = ref(
-  [{ name: "Birthday", description: "Celebrate this months birthday", category:"Math", 
+  [{ name: "Birthday", description: "Celebrate this months birthday", category:"Math", location: "Garvey Center",
       title: "birthday",
       start: new Date('Feb 1, 2025 12:00 PM'),
       end: new Date('Feb 1, 2025 12:10 PM'),
       color: 'blue',
+      event: 'SelectDay()',
+      href: '/google',
+      link: true
   },
   
   
@@ -24,8 +27,20 @@ const events = ref(
 
   const SelectDay = () =>
   {
-    calDialog = true;
+    calDialog.value = true;
   }
+
+  const findElementIndex = (event) => {
+      const clickedElement = event.target.closest('.v-chip__content');
+      if (!clickedElement) {
+        console.log('Clicked element is not a .v-chip__content element');
+        return;
+      }
+      const allElements = Array.from(document.querySelectorAll('.v-chip__content'));
+      const elementIndex = allElements.indexOf(clickedElement);
+      console.log('Clicked .v-chip__content Element Index:', elementIndex);
+      SelectDay();
+    }
 
 </script>
 
@@ -42,9 +57,13 @@ const events = ref(
                     ref="calendar"
                     :events="events"
                     type="month"
-                    @click:date="SelectDay()"
+                    @click="findElementIndex(clickedElement)"
                     
-                  ></v-calendar>
+                  >
+                  <template v-slot:day="day" ref="day" class="cursor-pointer">
+                    <div class="tw-h-full" @click="SelectDay()"></div>
+                  </template>
+                </v-calendar>
                 </v-sheet>
               </v-col>
             </v-row>
@@ -60,28 +79,26 @@ const events = ref(
               max-width="600"
               min-width="400"
             >
-                <v-card-title class="text-center">
+                <v-card-title class="text-center text-h4">
                   {{events[currentItem].name}}
                 </v-card-title>
-                <v-card-subtitle class="text-center">
-                  {{events[currentItem].category}}
-                </v-card-subtitle>
                 <v-card-text>
                   {{events[currentItem].description}}
                 </v-card-text>
                 <v-card-text>
+                  <v-icon>mdi-calendar</v-icon>
                   {{formatter.format(events[currentItem].start, 'shortDate')}}, 
                   {{formatter.format(events[currentItem].start, 'fullTime12h')}} - 
                   {{formatter.format(events[currentItem].end, 'fullTime12h')}}
-                  
+                  <v-spacer></v-spacer>
+                  <v-icon>mdi-map-marker</v-icon>
+                  {{events[currentItem].location}}
+                  <v-spacer></v-spacer>
+                  <v-icon>mdi-shape</v-icon>
+                  {{events[currentItem].category}}
                 </v-card-text>
-                <v-card-text>
-                  {{events[currentItem].start.getUTCDate()}}
-                </v-card-text>
-
-            
               <template v-slot:actions>
-                <v-btn class="ms-auto" text="Cancel" @click="calDialog = false"></v-btn>
+                <v-btn class="ms-auto" text="Close" @click="calDialog = false"></v-btn>
               </template>
             </v-card>
           </v-dialog>
@@ -132,9 +149,8 @@ const events = ref(
 .badge-image{
   max-height:10%;
 }
-.main-tasks{
-  
-}
+
+
 
 
 </style>
