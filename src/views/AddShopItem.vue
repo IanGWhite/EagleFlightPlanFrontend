ade7d9ba1fb81b8831be17c0e7d442a3581d5605
ChargeChan <chandlerbilbo@gmail.com>
2025-04-10 16:13:32 -0500

    more setup

--- src/views/AddShopItem.vue
+++ src/views/AddShopItem.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
-import ExperienceServices from "../services/experienceServices.js"; // hypothetical service for managing experience data
 import MenuBar from "../components/MenuBar.vue";
 import Utils from "../config/utils.js";
 import shopItemServices from "../services/shopItemServices.js";
 
 const router = useRouter();
 const user = ref({});
+
+const shopItems = ref(
+  [{ name: "", points: 0, description: "", imageLink:""},
+  ]);
 
 const shopItem = ref({
   name: "", 
@@ -17,11 +20,22 @@ const shopItem = ref({
 });
 const message = ref("");
 
-const saveShopItem = () => {
+const saveShopItem5 = () => {
   ExperienceServices.createExperience(user.value.studentId,experience.value)
     .then(() => {
       message.value = "Experience saved successfully";
       router.push({ name: "StudentInfo" }); // hypothetical route name for experience list
+    })
+    .catch((e) => {
+      message.value = "An error occurred";
+    });
+};
+
+const saveShopItem = () => {
+  shopItemServices.createShopItem(shopItem)
+    .then(() => {
+      message.value = "Experience saved successfully";
+      cancel();
     })
     .catch((e) => {
       message.value = "An error occurred";
@@ -34,8 +48,21 @@ const cancel = () => {
 
 onMounted(() => {
   user.value = Utils.getStore('user')
+  // fetchShopItems();
   console.log(user.value)
 })
+
+// const fetchShopItems = () => {
+//   shopItemServices.getAllShopItems()
+//     .then((response) => {
+//       shopItems.value = response.data; // Assuming the backend returns an array of events
+//       console.log("Fetched shop items:", shopItems.value);
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching shop items:", error);
+//     });
+    
+// };
 </script>
 
 <template>
@@ -43,52 +70,53 @@ onMounted(() => {
     <v-container>
       <v-card>
         <v-card-title class="text-center">
-          Experience
+          Add Shop Item
         </v-card-title>
         <v-card-text>
           <p>{{ message }}</p>
           <v-form>
             <v-text-field
-              v-model="experience.name"
-              label="Organization Name"
+              v-model="shopItem.name"
+              label="Item Name"
               required
             ></v-text-field>
 
             <v-text-field
-              v-model="experience.position"
-              label="Position Title"
+              v-model="shopItem.points"
+              label="Points"
+              type="number"
               required
             ></v-text-field>
 
-            <v-text-field
-              v-model="experience.state"
+            <!-- <v-text-field
+              v-model="shopItem.state"
               label="State"
               required
-            ></v-text-field>
+            ></v-text-field> -->
 
             <div class="row">
-              <v-text-field
-                v-model="experience.startDate"
+              <!-- <v-text-field
+                v-model="shopItem.startDate"
                 label="Start Month"
                 class="mr-2"
                 required
               ></v-text-field>
               <v-text-field
-                v-model="experience.endDate"
+                v-model="shopItem.endDate"
                 label="End Month"
                 required
-              ></v-text-field>
+              ></v-text-field> -->
             </div>
 
             <v-textarea
-              v-model="experience.description"
+              v-model="shopItem.description"
               label="Description"
               rows="4"
             ></v-textarea>
 
             <div class="buttons">
-              <v-btn color="red" @click="saveExperience">Save</v-btn>
               <v-btn color="error" @click="cancel">Cancel</v-btn>
+              <v-btn color="red" type="submit">Save</v-btn>
             </div>
           </v-form>
         </v-card-text>
